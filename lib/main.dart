17bb9db1888bfac23ce3a65e17ddecd359ac3dbb
champaksworldcreate <champaksworld@gmail.com>
2023-03-05 18:44:02 +0530

    multiple columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: true,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.tealAccent,
+          appBar: AppBar(
+            backgroundColor: Colors.blueAccent,
+            centerTitle: true,
+            title: const Text("Hello"),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.cyanAccent,
+                child: Text("One"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.cyanAccent,
+                child: Text("Two"),
+              ),
+              Container(
+                color: Colors.cyanAccent,
+                child: Text("Three"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
