e2af11dee646be6338dd5b2c363cbc66cc351b4b
champaksworldcreate <champaksworld@gmail.com>
2023-12-01 10:29:58 +0530

    Intl Phone Number Input
    A simple and customizable flutter package for inputting phone number in intl / international format uses Google's libphonenumber

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:card_slider/card_slider.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-void main() {
-  runApp(const MaterialApp(home: MainPage()));
-}
+void main() => runApp(MyApp());
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Color> valuesDataColors = [
-      Colors.purple,
-      Colors.yellow,
-      Colors.green,
-      Colors.red,
-      Colors.grey,
-      Colors.blue,
-    ];
+    var darkTheme = ThemeData.dark().copyWith(primaryColor: Colors.blue);
 
-    List<Widget> valuesWidget = [];
-    for (int i = 0; i < valuesDataColors.length; i++) {
-      valuesWidget.add(Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.0),
-            color: valuesDataColors[i],
-          ),
-          child: Align(
-            alignment: Alignment.center,
-            child: Text(
-              i.toString(),
-              style: const TextStyle(
-                fontSize: 28,
-              ),
-            ),
-          )));
-    }
-
-    return Scaffold(
-      backgroundColor: const Color(0xFF1560BD),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text(
-          "Cards",
-          style: TextStyle(
-              fontSize: 14, fontWeight: FontWeight.w600, color: Colors.white),
-        ),
-        centerTitle: true,
+    return MaterialApp(
+      title: 'Demo',
+      themeMode: ThemeMode.dark,
+      darkTheme: darkTheme,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: CardSlider(
-        cards: valuesWidget,
-        bottomOffset: .0003,
-        cardHeight: 0.75,
-        containerHeight: MediaQuery.of(context).size.height - 100,
-        itemDotOffset: -0.05,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Demo')),
+        body: MyHomePage(),
       ),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final TextEditingController controller = TextEditingController();
+  String initialCountry = 'IN';
+  PhoneNumber number = PhoneNumber(isoCode: 'IN');
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InternationalPhoneNumberInput(
+              onInputChanged: (PhoneNumber number) {
+                print(number.phoneNumber);
+              },
+              onInputValidated: (bool value) {
+                print(value);
+              },
+              selectorConfig: SelectorConfig(
+                selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+              ),
+              ignoreBlank: false,
+              autoValidateMode: AutovalidateMode.disabled,
+              selectorTextStyle: TextStyle(color: Colors.black),
+              initialValue: number,
+              textFieldController: controller,
+              formatInput: true,
+              keyboardType:
+              TextInputType.numberWithOptions(signed: true, decimal: true),
+              inputBorder: OutlineInputBorder(),
+              onSaved: (PhoneNumber number) {
+                print('On Saved: $number');
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                formKey.currentState?.validate();
+              },
+              child: Text('Validate'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                getPhoneNumber('+9335874326');
+              },
+              child: Text('Update'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                formKey.currentState?.save();
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void getPhoneNumber(String phoneNumber) async {
+    PhoneNumber number =
+    await PhoneNumber.getRegionInfoFromPhoneNumber(phoneNumber, 'IN');
+
+    setState(() {
+      this.number = number;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
